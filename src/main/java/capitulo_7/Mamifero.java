36bcf9bc9a38bc088b8356ca4eae20a9f2dab920
merlinuenp <josermerlin@gmail.com>
2022-07-12 12:13:44 -0300

    Herança.

---
+++ src/main/java/capitulo_7/Mamifero.java
@@ -0,0 +1,8 @@
+package capitulo_7;
+
+class Mamifero {
+    
+    public Mamifero (int idade){
+        System.out.println("Mamífero");
+    }
+}
---
+++ src/main/java/capitulo_7/Ornitorrinco.java
@@ -0,0 +1,14 @@
+package capitulo_7;
+
+
+public class Ornitorrinco extends Mamifero {
+    
+    public Ornitorrinco(){
+        System.out.println("Ornitorrinco");
+    }
+
+    
+    public static void main(String[] args) {
+        new Mamifero(5); 
+    }
+}
